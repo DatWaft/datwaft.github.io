b52d2afe968711302e2d8c2cc11374c34be9dca2
datwaft <datwaftx@gmail.com>
2020-11-28 21:08:23 -0600

    Better WIP message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <NavBar :title="title" :options="options" />
-  <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
-    <div style="font-size: 5rem;">This site is WIP</div>
+  <div style="display: flex; justify-content: center; align-items: center; flex-grow: 1;">
+    <div style="
+      font-size: 5rem;
+      user-select: none;
+      opacity: 25%;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+      This site is WIP
+    </div>
   </div>
 </template>
 
@@ -36,7 +42,10 @@ export default {
 
 <style>
 #app {
-  height: 100%;
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column;
 }
 
 :root,
@@ -46,5 +55,7 @@ body {
 
 body {
   margin: 0;
+
+  display: flex;
 }
 </style>
